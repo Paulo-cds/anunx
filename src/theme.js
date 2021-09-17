ce162279e62c43f163718c118ce282198981a401
paulo-cds <paulo.pdomingues@hotmail.com>
2021-09-17 15:42:17 -0300

    estrutura inicial + material-ui

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+import {createTheme} from '@material-ui/core/styles'
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main:'#000000',
+        }
+    }
+})
+
+
+export default theme
