cd83a1ffe77a51603cabe07b728811b100be173f
paulo-cds <paulo.pdomingues@hotmail.com>
2021-09-24 11:23:12 -0300

    adicionando feature de upload de imagens (front)

--- pages/user/publish.js
+++ pages/user/publish.js
@@ -1,10 +1,23 @@
-import { Container, TextField, Typography, Select, Button } from '@mui/material'
+import {
+    Container,
+    TextField,
+    Typography,
+    Select,
+    Button,
+    IconButton
+} from '@mui/material'
+
+import {useDropzone} from 'react-dropzone'
 import TemplateDefault from '../../src/templates/Default'
 import { makeStyles, } from '@mui/styles'
 import { Box } from '@mui/system'
-//import theme from '../../src/theme'
+import { DeleteForever } from '@material-ui/icons'
+import { useState } from 'react'
+
 
 const useStyles = makeStyles((theme) => ({
+    mask:{},
+    mainImage:{},
     container: {
         padding: theme.spacing(8, 0, 6),
     },
@@ -15,13 +28,83 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.background.white,
         padding: theme.spacing(3),
     },    
+    thumbsContainer:{
+        display: 'flex',
+        flexWrap: 'wrap',
+        marginTop: 15,
+        
+    },
+    dropzone:{
+        cursor: 'pointer',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        padding: 10,
+        margin: '0 15px 15px 0',
+        width: 200,
+        height: 150,
+        backgroundColor: theme.palette.background.default,
+        border: '2px dashed black',
+    },
+    thumb:{
+        position: 'relative',
+        
+        width: 200,
+        height: 150,
+        backgroundSize: 'cover',
+        margin: '0 15px 15px 0',
+        backgroundPosition: 'center center',
+
+        '& $mainImage': {
+            backgroundColor: 'blue',
+            padding: '6px 10px',
+            position: 'absolute',
+            bottom:0,
+            left:0,
+        },
+
+        '&:hover $mask':{
+            display: 'flex',
+        },
+
+        '& $mask':{
+            display: 'none',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            backgroundColor: 'rgba(0,0,0,0.7)',
+            width: '100%',
+            height: '100%',
+        }
+    }
     
 }))
 
 const Publish = () => {
     const classes = useStyles()
+    const [files, setFiles] = useState([])
     
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: 'image/*',
+        onDrop:(acceptedFile) => {
+            const newFiles = acceptedFile.map(file => {
+                return Object.assign(file, {
+                    preview: URL.createObjectURL(file)
+                })
+            })
 
+            setFiles([
+                ...files,
+                ...newFiles
+            ])
+        }
+    })
+
+    const handleRemoveFile = fileName => {
+        const newFileState = files.filter(file => file.name !== fileName)
+        setFiles(newFileState)
+    }
 
     return(
         <TemplateDefault >
@@ -88,6 +171,42 @@ const Publish = () => {
                     <Typography component='div' variant='body2' color='textPrimary'>
                         A primeira imagem é a foto principal do seu anúncio.
                     </Typography>
+                    <Box className={classes.thumbsContainer}>
+                        <Box className={classes.dropzone} {...getRootProps()}>
+                            <input {...getInputProps()}/>
+                            <Typography variant='body2' color='textPrimary'>
+                                Clique para adicionar ou arraste a imagem para aqui.
+                            </Typography>
+                        </Box>
+
+                        {
+                            files.map((file, index) => (
+                                <Box 
+                                    key={file.name}
+                                    className={classes.thumb}
+                                    style={{backgroundImage:`url(${file.preview})`}}
+                                >
+                                    {
+                                        index === 0 ?
+                                        <Box className={classes.mainImage}>
+                                            <Typography variant='body2' color='secondary'>
+                                                Principal
+                                            </Typography>
+                                        </Box>
+                                        : null
+                                    }
+                                    <Box className={classes.mask}>
+                                        <IconButton color='secondary' onClick={() => handleRemoveFile(file.name)}>
+                                            <DeleteForever fontSize='large'/>
+                                        </IconButton>
+                                    </Box>
+                                </Box>
+                            ))
+                        }
+
+                        
+
+                    </Box>
                 </Box>
             </Container>
 
